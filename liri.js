706cea8e493434436da4106e898581a4093df251
Martin Polder <martinpolder@gmail.com>
2019-08-14 15:16:03 -0400

    MVP done

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
 var axios = require("axios");
 
 
@@ -13,16 +14,18 @@ var media = process.argv[3];
 switch (commands) {
     case "concert-this":
         findGig();
-        console.log("concert-this" + media);
+        console.log("concert-this");
         break;
     case "spotify-this-song":
-        console.log("spotify-this-song" + media);
+        findSong();
+        console.log("spotify-this-song");
         break;
     case "movie-this":
         findMovie();
-        console.log("movie-this" + media);
+        console.log("movie-this");
         break;
     case "do-what-it-says":
+        doIt();
         console.log("do-what-it-says" + "random.txt");
         break;
 
@@ -33,7 +36,7 @@ switch (commands) {
 };
 
 
-
+// CONCERT SEARCH FUNCTION
 function findGig() {
     if (media != null) {
         var gig = media;
@@ -51,11 +54,14 @@ function findGig() {
 
         function (response) {
             console.log("response-test:")
-            console.log(response.data.offers)
+            // always comes up undefined >:(
+            console.log(response.data.venues)
         });
 
 };
 
+
+// MOVIE SEARCH FUNCTION
 function findMovie() {
     if (media != null) {
         var movie = media;
@@ -94,12 +100,85 @@ function findMovie() {
 
 };
 
-// function findSong (){
 
-// };
+// SPOTIFY SEARCH FUNCTION
+function findSong() {
+
+    if (media != null) {
+        var song = media;
+    }
+    else {
+        var song = "This Will Be Our Year"
+    }
+
+
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        else {
+            var songInfo = data.tracks.items;
+
+            console.log("<<<<<<<<ARTIST:>>>>>>>>");
+            console.log(songInfo[0].artists[0].name);
+            console.log("<<<<<<<<SONG:>>>>>>>>");
+            console.log(songInfo[0].name);
+            console.log("<<<<<<<<PREVIEW:>>>>>>>>");
+            console.log(songInfo[0].external_urls.spotify);
+            console.log("<<<<<<<<ALBUM:>>>>>>>>");
+            console.log(songInfo[0].album.name);
+        }
+
+
+    });
+
+};
 
 
 
-// function doIt (){
+function doIt() {
 
-// };
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // We will then print the contents of data
+        console.log(data);
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        // We will then re-display the content as an array for later use.
+
+        console.log(dataArr[1])
+        var thatway = dataArr[1];
+
+        spotify.search({ type: 'track', query: thatway }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            else {
+                var songInfo = data.tracks.items;
+
+                console.log("<<<<<<<<ARTIST:>>>>>>>>");
+                console.log(songInfo[0].artists[0].name);
+                console.log("<<<<<<<<SONG:>>>>>>>>");
+                console.log(songInfo[0].name);
+                console.log("<<<<<<<<PREVIEW:>>>>>>>>");
+                console.log(songInfo[0].external_urls.spotify);
+                console.log("<<<<<<<<ALBUM:>>>>>>>>");
+                console.log(songInfo[0].album.name);
+            }
+
+
+        });
+
+
+    });
+
+};
